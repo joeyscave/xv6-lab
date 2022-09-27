dfc4a39b48cc3e792ea4b008d9fd725c1c18fe73
200110213 <200110213@comp6.hwccluster.labcenter>
2022-09-27 15:34:41 +0000

    lab traps done

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -11,10 +11,10 @@ uint64
 sys_exit(void)
 {
   int n;
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -33,7 +33,7 @@ uint64
 sys_wait(void)
 {
   uint64 p;
-  if(argaddr(0, &p) < 0)
+  if (argaddr(0, &p) < 0)
     return -1;
   return wait(p);
 }
@@ -44,10 +44,10 @@ sys_sbrk(void)
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -58,12 +58,16 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-  if(argint(0, &n) < 0)
+  backtrace();
+
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
       release(&tickslock);
       return -1;
     }
@@ -78,7 +82,7 @@ sys_kill(void)
 {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
@@ -94,4 +98,46 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64
+sys_sigalarm()
+{
+  struct proc *p = myproc();
+  int ticks;
+  uint64 handler;
+
+  if (argint(0, &ticks) < 0)
+    return -1;
+  if (argaddr(1, &handler) < 0)
+    return -1;
+
+  p->alarmframe->interval = ticks;
+  p->alarmframe->handler = handler;
+
+  return 0;
+}
+
+uint64
+sys_sigreturn()
+{
+  struct proc *p = myproc();
+  uint64 *ap = (uint64 *)p->alarmframe;
+  uint64 *tp = (uint64 *)p->trapframe;
+
+  ap += 3;
+  tp += 3;
+  *tp = *ap; // trapframe->epc = alarmframe->epc
+  tp++;
+  ap++;
+
+  // resume other registers
+  for (int i = 0; i < 31; i++)
+  {
+    *(++tp) = *(++ap);
+  }
+
+  p->alarmframe->returned = 0;
+
+  return 0;
 }
